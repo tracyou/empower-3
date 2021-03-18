07e8a0e2abb3823542ef8656e73750ce9ccb2b3f
T.S.K. Owusu <tracy.owusu@hva.nl>
2021-03-18 01:55:04 +0100

    start an form

---
+++ src/app/local initiative/stakeholder-question9/stakeholder-question9.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stakeholder-question9',
+  templateUrl: './stakeholder-question9.component.html',
+  styleUrls: ['./stakeholder-question9.component.css']
+})
+export class StakeholderQuestion9Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
