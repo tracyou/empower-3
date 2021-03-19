7fc04419295a9d1e0c483e8dfa24064b25d73caf
T.S.K. Owusu <tracy.owusu@hva.nl>
2021-03-19 23:08:22 +0100

    bijna klaar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StakeholderQuestion9Component } from './local initiative/stakeholder-question9/stakeholder-question9.component';
+import { StakeholderQuestion10Component } from './stakeholder-question10/stakeholder-question10.component';
+import { StakeholderQuestion11Component } from './stakeholder-question11/stakeholder-question11.component';
+import { StakeholderQuestion12Component } from './stakeholder-question12/stakeholder-question12.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StakeholderQuestion9Component
+    StakeholderQuestion9Component,
+    StakeholderQuestion10Component,
+    StakeholderQuestion11Component,
+    StakeholderQuestion12Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/stakeholder-question12/stakeholder-question12.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stakeholder-question12',
+  templateUrl: './stakeholder-question12.component.html',
+  styleUrls: ['./stakeholder-question12.component.css']
+})
+export class StakeholderQuestion12Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
