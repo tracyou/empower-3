36b5d024bab41cb2c0f6593b68e9765d37ce6b6f
balhast <tahera.balhas@hva.nl>
2021-03-23 10:30:17 +0100

    minor change in path(routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { StakeholderQuestionsComponent } from './components/localInitiatives/sta
 import { JourneyAssetsComponent } from './components/localInitiatives/journey-assets/journey-assets.component';
 
 const appRoutes: Routes = [
+  { path : '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'localInitiative', component: MapComponent},
   { path: 'objectives', component: ObjectivesComponent},
