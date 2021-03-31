9f60026d234d621911021f23138685ea321b9d5d
TM van Breukelen <tessa.van.breukelen@hva.nl>
2021-03-31 20:19:27 +0200

    little problem with navbar fixed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,32 +5,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MapComponent } from './components/localInitiatives/map/map.component';
-import { ObjectivesComponent } from './components/localInitiatives/objectives/objectives.component';
 import {FormsModule} from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import {RouterModule, Routes} from '@angular/router';
-import { StakeholderQuestionsComponent } from './components/localInitiatives/stakeholder-questions/stakeholder-questions.component';
-import { JourneyAssetsComponent } from './components/localInitiatives/journey-assets/journey-assets.component';
 
 const appRoutes: Routes = [
   { path : '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'localInitiative', component: MapComponent},
-  { path: 'objectives', component: ObjectivesComponent},
-  { path: 'stakeholders', component: StakeholderQuestionsComponent},
-  { path: 'journeyAssets', component: JourneyAssetsComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     FooterComponent,
     MapComponent,
-    ObjectivesComponent,
     HomeComponent,
-    StakeholderQuestionsComponent,
-    JourneyAssetsComponent
+    NavbarComponent
   ],
 
   imports: [
