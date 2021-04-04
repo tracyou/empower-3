682abd79cb1c79d3230a3a049dc54edd7d52a5cd
T.S.K. Owusu <tracy.owusu@hva.nl>
2021-04-04 14:26:28 +0200

    start aan backend

--- backend/src/main/java/nl/hva/fdmci/backend/BackendApplication.java
+++ backend/src/main/java/nl/hva/fdmci/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package nl.hva.fdmci.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping
+	public String test(){
+	  return "test test 123";
+  }
 }
