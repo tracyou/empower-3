9a6002bc6e03245cca6551c8cb28d9501fa8c74c
balhast <tahera.balhas@hva.nl>
2021-04-12 18:40:34 +0300

    fixed constructors

--- backend/src/main/java/nl/hva/fdmci/backend/models/Initiative.java
+++ backend/src/main/java/nl/hva/fdmci/backend/models/Initiative.java
@@ -27,43 +27,45 @@ public class Initiative {
   private String state;
   private String zip;
 
+  public Initiative(int id, String city, String collaboration, Date date, String goalCreate, String gaolFootprint,
+                    String gaolGreen, String gaolImprove, String gaolReduce, String gridExpertise1,
+                    String gridExpertise2, String gridExpertise3, String gridRadios, String gridRadios2,
+                    String gridRadios3, String gridRadios4, String inputEmail, int inputPhone, String inputWebsite,
+                    String name, String selectedTheme, String state, String zip) {
+    this.id = id;
+    this.city = city;
+    this.collaboration = collaboration;
+    this.date = date;
+    this.goalCreate = goalCreate;
+    this.gaolFootprint = gaolFootprint;
+    this.gaolGreen = gaolGreen;
+    this.gaolImprove = gaolImprove;
+    this.gaolReduce = gaolReduce;
+    this.gridExpertise1 = gridExpertise1;
+    this.gridExpertise2 = gridExpertise2;
+    this.gridExpertise3 = gridExpertise3;
+    this.gridRadios = gridRadios;
+    this.gridRadios2 = gridRadios2;
+    this.gridRadios3 = gridRadios3;
+    this.gridRadios4 = gridRadios4;
+    this.inputEmail = inputEmail;
+    this.inputPhone = inputPhone;
+    this.inputWebsite = inputWebsite;
+    this.name = name;
+    this.selectedTheme = selectedTheme;
+    this.state = state;
+    this.zip = zip;
+  }
 
   public Initiative() {
 
-  };
-
-//  public Initiatative(int id, String city, String collaboration, Date date, String goalCreate, String gaolFootprint, String gaolGreen, String gaolImprove, String gaolReduce, String gridExpertise1, String gridExpertise2, String gridExpertise3, String gridRadios, String gridRadios2, String gridRadios3, String gridRadios4, String inputEmail, int inputPhone, String inputWebsite, String name, String selectedTheme, String state, String zip) {
-//    this.id = id;
-//    this.city = city;
-//    this.collaboration = collaboration;
-//    this.date = date;
-//    this.goalCreate = goalCreate;
-//    this.gaolFootprint = gaolFootprint;
-//    this.gaolGreen = gaolGreen;
-//    this.gaolImprove = gaolImprove;
-//    this.gaolReduce = gaolReduce;
-//    this.gridExpertise1 = gridExpertise1;
-//    this.gridExpertise2 = gridExpertise2;
-//    this.gridExpertise3 = gridExpertise3;
-//    this.gridRadios = gridRadios;
-//    this.gridRadios2 = gridRadios2;
-//    this.gridRadios3 = gridRadios3;
-//    this.gridRadios4 = gridRadios4;
-//    this.inputEmail = inputEmail;
-//    this.inputPhone = inputPhone;
-//    this.inputWebsite = inputWebsite;
-//    this.name = name;
-//    this.selectedTheme = selectedTheme;
-//    this.state = state;
-//    this.zip = zip;
-//  }
-  public Initiative(int id, String city) {
-    this.id = id;
-    this.city = city;
   }
 
   public static Initiative createRandom(int id){
-    return new Initiative(id, "city");
+    return new Initiative(id, "city", null, null, null, null, null,
+      null, null, null, null, null, null,
+      null, null, null, null, 0, null, null,
+      null, null, null);
   }
 
   public int getId() {
