3befa37a555a29a5271c55853d1deeee9dbae054
T.S.K. Owusu <tracy.owusu@hva.nl>
2021-05-27 15:25:55 +0200

    geprobeerd te connecten met frontend

--- backend/src/main/java/nl/hva/fdmci/backend/BackendApplication.java
+++ backend/src/main/java/nl/hva/fdmci/backend/BackendApplication.java
@@ -1,11 +1,7 @@
 package nl.hva.fdmci.backend;
 
-import nl.hva.fdmci.backend.models.User;
-import nl.hva.fdmci.backend.repositories.UserRepositorie;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -15,11 +11,11 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner commandLineRunner(UserRepositorie userRepository){
-	  return args -> {
-      User tracy = new User("Tracy", "Tracy123", "something", "title", "description");
-      userRepository.save(tracy);
-    };
-	}
+//	@Bean
+//	CommandLineRunner commandLineRunner(UserRepositorie userRepository){
+//	  return args -> {
+//      User tracy = new User("Tracy", "Tracy123", "something", "title", "description");
+//      userRepository.save(tracy);
+//    };
+//	}
 }
